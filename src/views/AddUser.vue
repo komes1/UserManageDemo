27ac1b45e4dd657c5a1dd8b83fa84ad4e5ff7972
komes1 <166900013+komes1@users.noreply.github.com>
2024-09-18 10:39:56 +0800

    Update AddUser.vue

--- src/views/AddUser.vue
+++ src/views/AddUser.vue
@@ -1,9 +1,9 @@
 <template>
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" style="width:40%">
-        <el-form-item label="书名" prop="name">
+        <el-form-item label="姓名" prop="name">
             <el-input v-model="ruleForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="作者" prop="author">
+        <el-form-item label="工号" prop="author">
             <el-input v-model="ruleForm.author"></el-input>
         </el-form-item>
         <el-form-item>
@@ -24,11 +24,11 @@
                 },
                 rules: {
                     name: [
-                        { required: true, message: '请输入书名', trigger: 'blur' },
+                        { required: true, message: '请输入姓名', trigger: 'blur' },
                         { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
                     ],
                     author: [
-                        { required: true, message: '请输入作者', trigger: 'blur' },
+                        { required: true, message: '请输入工号', trigger: 'blur' },
                         { min: 2, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
                     ]
                 }
